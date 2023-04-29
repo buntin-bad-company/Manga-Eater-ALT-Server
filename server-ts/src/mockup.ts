523639fcdd16a1bbf7dabef4c6a509e3b844c6fe
Buntin-Synthia <aokitakumi72@gmail.com>
2023-04-29 22:42:34 +0900

    mockup enhanced

--- server-ts/src/mockup.ts
+++ server-ts/src/mockup.ts
@@ -1,14 +1,72 @@
 import express, { Application, Request, Response } from 'express';
 import * as util from './scrapeUtils';
 import fs from 'fs';
-import type { Config } from './scrapeUtils';
 
+interface ChannelInfo {
+  currentName: string;
+  alt?: string[];
+}
+interface Config {
+  token: string;
+  channel: {
+    current: string;
+    alt: string[];
+  };
+  channelNames?: ChannelInfo;
+}
 console.log('Manga Eater Server is Starting...\nThis is a mockup.ts');
 //listen to port 3000
 //print request body mock server
 const app: Application = express();
 const PORT = 3000;
 
+/* from here The mock functions of scrapeUtils.ts */
+const loadConf = <T>(): T => {
+  const config = JSON.parse(fs.readFileSync('./config.mock.json', 'utf8')) as T;
+  return config;
+};
+const writeConf = <T>(config: T) => {
+  fs.writeFileSync('./config.mock.json', JSON.stringify(config, null, 2));
+};
+const fetchChannelName = async (token: string, channelID: string) => {
+  switch (channelID) {
+    case '1092900938592829471':
+      return '漫画raw';
+    case '1092355756378030081':
+      return 'ゲーム';
+    case '1067928311310852126':
+      return 'lsmrd';
+    default:
+      //throw error
+      throw new Error('channelID is not valid');
+  }
+};
+const fetchChannels = async () => {
+  const config = loadConf<Config>();
+  const token = config.token;
+  const currentName = await fetchChannelName(token, config.channel.current);
+  const altNames = await Promise.all(
+    config.channel.alt.map(async (channelId) => {
+      const name = await fetchChannelName(token, channelId);
+      return name;
+    })
+  );
+  const newConfig: Config = { ...config };
+  const channelNames: ChannelInfo = { currentName, alt: altNames };
+  newConfig.channelNames = channelNames;
+  return newConfig;
+};
+const changeChannel_mock = (index: number) => {
+  const config = loadConf<Config>();
+  const newCurrent = config.channel.alt[index];
+  const prevCurrent = config.channel.current;
+  const newConfig = { ...config };
+  newConfig.channel.current = newCurrent;
+  newConfig.channel.alt[index] = prevCurrent;
+  writeConf(newConfig);
+};
+
+/* mock functions end */
 interface CorsFunc {
   (req: Request, res: Response, next: Function): void;
 }
@@ -42,18 +100,15 @@ app.post('/', async (req: Request, res: Response) => {
 });
 app.get('/channel', (req: Request, res: Response) => {
   //sleep for 1 sec
-  util.sleep(1000).then(() => {
-    res.send({
-      currentName: '漫画raw',
-      alt: [],
-    });
+  fetchChannels().then((config) => {
+    res.send(config.channelNames || { current: 'none' });
   });
 });
 app.post('/channel', async (req: Request, res: Response) => {
   console.log('req.body :', req.body);
   const { index } = req.body;
-  util.changeChannel(index);
-  util.fetchChannels().then((config) => {
+  changeChannel_mock(index);
+  fetchChannels().then((config) => {
     res.send(config.channelNames || { current: 'none' });
   });
 });
