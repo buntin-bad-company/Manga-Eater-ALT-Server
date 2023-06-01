e7fb565162a399213a56d767e49d9d825797a6dd
Buntin-Synthia <aokitakumi72@gmail.com>
2023-06-01 11:22:40 +0900

    サイトごとに関数切り分け。

--- server-ts/src/BCHelper.ts
+++ server-ts/src/BCHelper.ts
@@ -4,8 +4,8 @@
  */
 
 import ServerStatusManager from "./ServerStatusManager";
-import {Server} from "socket.io";
-import {sleep, singleTitleScrape, calcPer, scrapeTitlePage, generateRandomString, getTitleAndEpisodes, scrapeFromUrl, loadConf, Discord, log, writeRecord} from './complexUtils';
+import { Server } from "socket.io";
+import { sleep, singleTitleScrape, calcPer, scrapeTitlePage, generateRandomString, getTitleAndEpisodes, scrapeFromUrl, loadConf, Discord, log, writeRecord } from './complexUtils';
 
 export class BCHelper {
   public static version: string = '1.0.1';
@@ -16,15 +16,15 @@ export class BCHelper {
     this.startProcessingLoop();
   }
 
-  public addTask(url: string, channelId: string, type: string) {
+  public addTask (url: string, channelId: string, type: string) {
     const task: BCTask = {
       type,
       url,
       id: generateRandomString(10),
       channelId,
     }
-    log(`Adding Task: ${task.id}`);
-    console.log(`task: ${JSON.stringify(task, null, 4)}`)
+    log(`Adding Task: ${ task.id }`);
+    console.log(`task: ${ JSON.stringify(task, null, 4) }`)
     this.queue.push(task);
     if (!this.isProcessing) {
       this.processQueue();
@@ -32,7 +32,7 @@ export class BCHelper {
     return task.id;
   }
 
-  public get state() {
+  public get state () {
     return {
       version: BCHelper.version,
       queue: this.queue,
@@ -40,17 +40,17 @@ export class BCHelper {
     }
   }
 
-  private startProcessingLoop() {
+  private startProcessingLoop () {
     setInterval(() => {
       this.processQueue();
-    }, 1 * 60 * 1000);
+    }, 5 * 60 * 1000);
   }
 
   /**
    * タスクを処理。処理中のタスクがあれば何もしない。
    * @returns 
    */
-  private async processQueue() {
+  private async processQueue () {
     console.log('Checking BCHelper Queue...');
     if (this.isProcessing || this.queue.length === 0) {
       return;
@@ -71,33 +71,38 @@ export class BCHelper {
    * 実際にタスクを処理する関数。
    * @param task task
    */
-  private async helper(task: BCTask | undefined) {
+  private async helper (task: BCTask | undefined) {
     if (!task) {
       return;
     }
     //'deferred-fetch-push' : 'deferred-fetch'
-    const {type, channelId, url} = task;
+    const { type, channelId, url } = task;
     if (!type || !channelId || !url) {
-      log(`bch:[ERROR]-[helper] task is invalid. task: \n\`\`\`json${JSON.stringify(task, null, 4)}\`\`\``);
+      log(`bch:[ERROR]-[helper] task is invalid. task: \n\`\`\`json${ JSON.stringify(task, null, 4) }\`\`\``);
       return;
     }
     let ifPush = type.includes('push');
-    let ifFetch = type.includes('fetch');
+    if (url.includes('mangarawjp.io')) {
+      await this.mangarawjpio(url, task, channelId, type, ifPush);
+    }
+  }
+
+  private async mangarawjpio (url: string, task: BCTask, channelId: string, type: string, ifPush: boolean) {
     if (url.includes('chapter')) {
       singleTitleScrape(task, this.ssm, type, url, this.outDir, channelId, ifPush);
     } else {
-      const {title, urls} = await scrapeTitlePage(url);
+      const { title, urls } = await scrapeTitlePage(url);
       const pid = this.ssm.createFetchJob();
       this.ssm.setJobsTitle(pid, title);
       this.ssm.setJobsProgress(pid, 'Fetching title page...');
       const len = urls.length;
       for (let i = 0; i < len; i++) {
-        this.ssm.setJobsProgress(pid, `Fetching title page... ${calcPer(i + 1, len)}%`);
+        this.ssm.setJobsProgress(pid, `Fetching title page... ${ calcPer(i + 1, len) }%`);
         try {
-          await singleTitleScrape(task, this.ssm, type, urls[ i ], this.outDir, channelId, false);
+          await singleTitleScrape(task, this.ssm, type, urls[i], this.outDir, channelId, false);
         } catch (e) {
           e = e as Error;
-          log('[BCH]' + ` [ERROR]-[helper] ${JSON.stringify(e, null, 4)}`);
+          await log('[BCH]' + ` [ERROR]-[helper] ${ JSON.stringify(e, null, 4) }`);
         } finally {
           this.ssm.setJobsProgress(pid, 'wait 1 min...');
           await sleep(1000 * 60 * 1);
